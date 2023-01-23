f365fb23461b03fa6ade075c9cbd0a4471e0e360
hemg2 <hemg2@naver.com>
2023-01-23 16:58:52 +0900

    Initial Commit

---
+++ PlanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanApp
+//
+//  Created by 1 on 2023/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
