8b311516bd068f5f082839c4f7d5c9906acda838
hemg2 <hemg2@naver.com>
2023-01-25 22:28:04 +0900

    ago

--- PlanApp/CameraView/RecordViewController.swift
+++ PlanApp/CameraView/RecordViewController.swift
@@ -91,6 +91,67 @@ final class RecordViewController: UIViewController, UIImagePickerControllerDeleg
         navigations()
         layout()
         imageViews()
+        configureEditMode()
+    }
+    func navigations() {
+        view.backgroundColor = .systemBackground
+        title = "기록하기"
+        self.navigationItem.rightBarButtonItems = [naviBarButton,naviButton]
+    }
+    
+    func imageViews() {
+        view.addSubview(imageLabel)
+        imageLabel.translatesAutoresizingMaskIntoConstraints = false
+        imageLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        imageLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: imageLabel.bottomAnchor, constant: 15).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        view.addSubview(imageButton)
+        imageButton.translatesAutoresizingMaskIntoConstraints = false
+        imageButton.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        imageButton.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        imageButton.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        imageButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    func layout() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor, constant: 250).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        
+        view.addSubview(titleTextField)
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        titleTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        
+        view.addSubview(subTitleLabel)
+        subTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subTitleLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 30).isActive = true
+        subTitleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        
+        view.addSubview(descriptionTextField)
+        descriptionTextField.translatesAutoresizingMaskIntoConstraints = false
+        descriptionTextField.topAnchor.constraint(equalTo: subTitleLabel.bottomAnchor, constant: 10).isActive = true
+        descriptionTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+    }
+    
+    private func configureEditMode() {
+        switch self.listMode {
+        case let .edit(_, list):
+            self.titleTextField.text = list.title
+            self.descriptionTextField.text = list.description
+//            self.imageView.image = list.mainImage
+            
+        default:
+            break
+        }
     }
 }
 
