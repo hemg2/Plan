0115571378e09e6ed1a1f563999951cd930b95e1
hemg2 <hemg2@naver.com>
2023-01-25 15:42:01 +0900

    RecordView UI

--- PlanApp/CameraView/RecordViewController.swift
+++ PlanApp/CameraView/RecordViewController.swift
@@ -11,6 +11,27 @@ import UIKit
 final class RecordViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     private let imagePickerController = UIImagePickerController()
+    
+    lazy var imageView: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .systemBackground
+        return image
+    }()
+    
+    lazy var imageLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 35)
+        label.text = "사진"
+        return label
+    }()
+    
+    lazy var imageButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(nil, for: .normal)
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        return button
+    }()
+    
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 35)
@@ -20,13 +41,13 @@ final class RecordViewController: UIViewController, UIImagePickerControllerDeleg
     
     lazy var subTitleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 30)
+        label.font = UIFont.systemFont(ofSize: 35)
         label.text = "내용"
         return label
     }()
     
     lazy var textField: UITextField = {
-       let textField = UITextField()
+        let textField = UITextField()
         textField.frame = CGRect(x: 100, y: 100, width: 400, height: 30)
         textField.borderStyle = .roundedRect
         textField.placeholder = "제목을 입력해주세요."
@@ -35,10 +56,10 @@ final class RecordViewController: UIViewController, UIImagePickerControllerDeleg
     
     lazy var textView: UITextField = {
         let textField = UITextField()
-         textField.frame = CGRect(x: 100, y: 100, width: 400, height: 30)
-         textField.borderStyle = .roundedRect
-         textField.placeholder = "내용을 입력해주세요."
-         return textField
+        textField.frame = CGRect(x: 100, y: 100, width: 400, height: 30)
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "내용을 입력해주세요."
+        return textField
     }()
     
     lazy var naviButton: UIBarButtonItem = {
@@ -56,23 +77,49 @@ final class RecordViewController: UIViewController, UIImagePickerControllerDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        title = "기록중"
-        self.navigationItem.rightBarButtonItems = [naviButton, naviBarButton]
+        navigations()
         layout()
+        imageViews()
+    }
+    
+    func navigations() {
+        view.backgroundColor = .systemBackground
+        title = "기록하기"
+        self.navigationItem.rightBarButtonItems = [naviButton, naviBarButton]
+    }
+    
+    func imageViews() {
+        view.addSubview(imageLabel)
+        imageLabel.translatesAutoresizingMaskIntoConstraints = false
+        imageLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        imageLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: imageLabel.bottomAnchor, constant: 15).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        view.addSubview(imageButton)
+        imageButton.translatesAutoresizingMaskIntoConstraints = false
+        imageButton.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        imageButton.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        imageButton.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        imageButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
     }
     
     func layout() {
         view.addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor, constant: 300).isActive = true
+        titleLabel.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor, constant: 250).isActive = true
         titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
         
         view.addSubview(textField)
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
         textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
-
+        
         view.addSubview(subTitleLabel)
         subTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         subTitleLabel.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 30).isActive = true
