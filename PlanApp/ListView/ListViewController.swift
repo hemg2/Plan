b7921cd15bf8289eff93a697fe770e0854ff354a
hemg2 <hemg2@naver.com>
2023-01-31 21:21:27 +0900

    UserDefaults save  까지 가능

--- PlanApp/ListView/ListViewController.swift
+++ PlanApp/ListView/ListViewController.swift
@@ -11,11 +11,11 @@ final class ListViewController: UIViewController, UIImagePickerControllerDelegat
   
     private var targetModel = [TagetModel]()
     private var list = [ListModel]()
-//    {
-//        didSet {
-//            saveList()
-//        }
-//    }
+    {
+        didSet {
+            saveList()
+        }
+    }
     
     private var tableView: UITableView = {
         let tableView = UITableView()
@@ -41,6 +41,7 @@ final class ListViewController: UIViewController, UIImagePickerControllerDelegat
         navigationItem()
         tableViewLayout()
         tableViewExtension()
+        loadList()
         NotificationCenter.default.addObserver(self, selector: #selector(editDiaryNotification(_:)), name: NSNotification.Name("List"), object: nil)
     }
     
@@ -75,41 +76,23 @@ final class ListViewController: UIViewController, UIImagePickerControllerDelegat
     
     
     private func saveList() {
-        let date = self.list.map {
-            [
-                "title": $0.title,
-                "description": $0.description,
-                "mainImage": $0.mainImage,
-                "date": $0.date
-            ]
+    
+        let data = self.list.map {
+            $0.title
         }
+        
         let userDefaults = UserDefaults.standard
-        userDefaults.set(date, forKey: "list")
+        userDefaults.set(data, forKey: "data")
+        print("저장은 되는건가\(data)")
     }
     
-    private func saveTarget() {
-        let date = self.targetModel.map {
-            [
-                "title": $0.title,
-                "description": $0.description,
-                "date": $0.date
-            ]
-        }
+    func loadList() {
         let userDefaults = UserDefaults.standard
-        userDefaults.set(date, forKey: "target")
+        let data = userDefaults.object(forKey: "data") as? String
+        print("로드데이터\(data)")
     }
     
-//    func loadList() {
-//        let userDefaults = UserDefaults.standard
-//        guard let data = userDefaults.object(forKey: "list") as? [[String: Any]] else { return }
-//        self.list = data.compactMap {
-//            guard let title = $0["title"] as? String else { return }
-//            guard let description = $0["description"] as? String else { return }
-//            guard let mainImage = $0["mainImage"]as? UIImage else { return }
-//            guard let date = $0["date"] as? Date else { return }
-//            return ListModel(mainImage: mainImage, title: title, description: description, date: date)
-//        }
-//    }
+    
     
 //    func setUserDefaults(UIImage value: UIImage, _ key: String) {
 //        let imageData = value.jpegData(compressionQuality: 1.0)
@@ -241,12 +224,7 @@ extension ListViewController: UITableViewDelegate {
         })
         actions1.backgroundColor = .systemRed
         
-        let actions2 = UIContextualAction(style: .normal, title: "수정", handler: { [weak self] action, view, completionHaldler in completionHaldler(true)
-            
-        })
-        actions2.backgroundColor = .systemBlue
-        
-        return UISwipeActionsConfiguration(actions: [actions1, actions2])
+        return UISwipeActionsConfiguration(actions: [actions1])
     }
     
     
