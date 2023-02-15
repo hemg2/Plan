a73c08f92ad706d4bdff80e41e0e94bdcfd40395
hemg2 <hemg2@naver.com>
2023-02-15 23:32:59 +0900

    ago next button

--- PlanApp/ListView/Cell/MonthCell.swift
+++ PlanApp/ListView/Cell/MonthCell.swift
@@ -9,12 +9,40 @@ import UIKit
 
 
 final class MonthCell: UITableViewCell {
+    
+    var selectedDate = Date()
+    
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 25)
         return label
     }()
     
+    lazy var nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(nil, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setImage(UIImage(systemName: "arrowshape.right.fill"), for: .normal)
+        button.addTarget(self, action: #selector(nextWeek), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var agoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(nil, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setImage(UIImage(systemName: "arrowshape.left.fill"), for: .normal)
+        button.addTarget(self, action: #selector(agoWeek), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func nextWeek() {
+        selectedDate = CalendarHelper().addDays(date: selectedDate, days: 7) // 실패
+    }
+    
+    @objc private func agoWeek() {
+        
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -31,6 +59,17 @@ final class MonthCell: UITableViewCell {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        
+        contentView.addSubview(agoButton)
+        agoButton.translatesAutoresizingMaskIntoConstraints = false
+        agoButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+        agoButton.trailingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -10).isActive = true
+        
+        contentView.addSubview(nextButton)
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+        nextButton.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 10).isActive = true
     }
     
 }
